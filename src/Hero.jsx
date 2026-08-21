ef45ac556087b47aff003a19a0dc54b60b5d5e76
Sajida <sajidapm@gmail.com>
2026-08-22 01:50:58 +0530

    Hero section problem has been fixed.

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div>
       <NavLink to="/Productlist" >
-      <img src='public\ChatGPT Image Aug 14, 2026, 03_58_20 PM.png' className='md:h-125 w-full'></img></NavLink>
+      <img src='public/Hero.png' className='md:h-125 w-full'></img></NavLink>
     </div>
   )
 }
