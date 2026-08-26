c845f4d0673b8dd1340fe74d5e5ba0374ad9834c
Sajida <sajidapm@gmail.com>
2026-08-26 07:55:45 +0530

    edited mob nav alignment 3

--- src/Components/MobileFooter.jsx
+++ src/Components/MobileFooter.jsx
@@ -1,36 +1,64 @@
-import React from 'react'
-import {NavLink } from 'react-router-dom'
-import  { useContext } from 'react'
-import { Search, ShoppingCart ,ClipboardList, UserRound} from "lucide-react";
-import { House } from 'lucide-react'
-import { CartContext } from '../Context/CartContext';
-import { Heart} from 'lucide-react'
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import {
+  House,
+  Heart,
+  ShoppingCart,
+  ClipboardList,
+  UserRound,
+} from "lucide-react";
+import { CartContext } from "../Context/CartContext";
+
 const MobileFooter = () => {
   const { cart } = useContext(CartContext);
-  
+
   return (
-    <div>
-      <footer className='fixed bottom-0 left-0 right-0 z-50 bg-white  md:hidden mx-2'>
-      <div className='flex justify-between  '>
-        <NavLink to="/Hero"><div > <House size={24} className='text-blue-600' />
-            <div>Home</div></div></NavLink>
+    <footer className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t md:hidden pb-[env(safe-area-inset-bottom)]">
+      <div className="flex justify-around items-center h-16">
 
-        <NavLink to="/wishlistPage"><div > <Heart size={24} className='' />
-           <div>Wishlist</div></div></NavLink>
+        <NavLink to="/Hero">
+          <div className="flex flex-col items-center text-xs">
+            <House size={24} className="text-blue-600" />
+            <span>Home</span>
+          </div>
+        </NavLink>
 
-        <NavLink to="/Cart"><div className='relative'> <ShoppingCart size={24} className='' />
-           <div>Cart</div><div className='absolute bottom-10 left-5 text-xs w-5 h-5 
-             rounded-full bg-red-500 text-white text-center '>{cart.length}</div> </div></NavLink>
+        <NavLink to="/wishlistPage">
+          <div className="flex flex-col items-center text-xs">
+            <Heart size={24} />
+            <span>Wishlist</span>
+          </div>
+        </NavLink>
 
-        <NavLink to="/Orders"><div className='flex flex-col items-center'>
-            <ClipboardList size={24} /><div>Orders</div></div></NavLink>
+        <NavLink to="/Cart">
+          <div className="relative flex flex-col items-center text-xs">
+            <ShoppingCart size={24} />
 
-        <NavLink to="/Account"><div className='flex flex-col items-center'> 
-             <UserRound /><div>Account</div></div></NavLink>
+            <span>Cart</span>
+
+            <span className="absolute -top-1 -right-2 w-5 h-5 rounded-full bg-red-500 text-white text-xs flex items-center justify-center">
+              {cart.length}
+            </span>
+          </div>
+        </NavLink>
+
+        <NavLink to="/Orders">
+          <div className="flex flex-col items-center text-xs">
+            <ClipboardList size={24} />
+            <span>Orders</span>
+          </div>
+        </NavLink>
+
+        <NavLink to="/Account">
+          <div className="flex flex-col items-center text-xs">
+            <UserRound size={24} />
+            <span>Account</span>
+          </div>
+        </NavLink>
+
       </div>
-      </footer>
-    </div>
-  )
-}
+    </footer>
+  );
+};
 
-export default MobileFooter
+export default MobileFooter;
