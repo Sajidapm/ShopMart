f7447ec612a564b4967f164ff84b867506fb9346
Sajida <sajidapm@gmail.com>
2026-08-26 08:41:39 +0530

    edited mob nav alignment 4

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -78,7 +78,7 @@ const linkStyle = ({ isActive }) =>
     </nav> 
 
    {/* Navbar in Mobile...............................................*/}
-    <nav className='md:hidden pb-3  relative mx-1 mr-3'>
+    <nav className='md:hidden pb-3 my-7 relative mx-1 mr-3'>
       {/*Align Logo and Menu... */}
       <div className='flex  justify-between'>
       {/*logo....... */}
